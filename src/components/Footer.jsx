8631d1d252a4b3a69e2c6d32e4a37d416f6ffe28
taeinha <taeinha@gmail.com>
2020-10-22 22:26:44 -0400

    Fix footer color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,22 +5,22 @@ const Footer = props => {
     <ul className="footer-container">
       <li>Developed by Tae Ha</li>
       <li>
-        <a href="https://github.com/taeinha/pnp_tasks_taeha" target="_blank" rel="noopener noreferrer" >
+        <a href="https://github.com/taeinha/pnp_tasks_taeha" target="_blank" rel="noopener noreferrer" className="footer-icon" >
           <i className="fab fa-github-square"></i>
         </a>
       </li>
       <li>
-        <a href="https://taeinha.com/" target="_blank" rel="noopener noreferrer" >
+        <a href="https://taeinha.com/" target="_blank" rel="noopener noreferrer" className="footer-icon">
           <i className="fas fa-suitcase"></i>
         </a>
       </li>
       <li>
-        <a href="https://angel.co/tae-in-ha" target="_blank" rel="noopener noreferrer" >
+        <a href="https://angel.co/tae-in-ha" target="_blank" rel="noopener noreferrer" className="footer-icon">
           <i className="fab fa-angellist"></i>
         </a>
       </li>
       <li>
-        <a href="https://www.linkedin.com/in/tae-in-ha/" target="_blank" rel="noopener noreferrer" >
+        <a href="https://www.linkedin.com/in/tae-in-ha/" target="_blank" rel="noopener noreferrer" className="footer-icon">
           <i className="fab fa-linkedin"></i>
         </a>
       </li>
