78833007ae788066140f4d6948a3a62e464126e2
taeinha <taeinha@gmail.com>
2020-10-22 22:17:42 -0400

    Fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,26 @@ const Footer = props => {
   return (
     <ul className="footer-container">
       <li>Developed by Tae Ha</li>
-      <li><a href="https://github.com/taeinha/pnp_tasks_taeha" target="_blank" rel="noopener noreferrer" ><i className="fab fa-github-square"></i></a></li>
-      <li><a href="https://taeinha.com/" target="_blank" rel="noopener noreferrer" ><i className="fas fa-suitcase"></i></a></li>
-      <li><a href="https://angel.co/tae-in-ha" target="_blank" rel="noopener noreferrer" ><i className="fab fa-angellist"></i></a></li>
-      <li><a href="https://www.linkedin.com/in/tae-in-ha/" target="_blank" rel="noopener noreferrer" ><i className="fab fa-linkedin"></i></a></li>
+      <li>
+        <a href="https://github.com/taeinha/pnp_tasks_taeha" target="_blank" rel="noopener noreferrer" >
+          <i className="fab fa-github-square"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://taeinha.com/" target="_blank" rel="noopener noreferrer" >
+          <i className="fas fa-suitcase"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://angel.co/tae-in-ha" target="_blank" rel="noopener noreferrer" >
+          <i className="fab fa-angellist"></i>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/tae-in-ha/" target="_blank" rel="noopener noreferrer" >
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </li>
     </ul>
   )
 };
