1ff411546abf8bf57d7c5fa7c9b338a4ac0a3ded
taeinha <taeinha@gmail.com>
2020-10-22 21:30:58 -0400

    Updates

---
+++ precache-manifest.3d8b8345f912e1bd002c79fc323d5c70.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8beb2b1f590c661460e12ce854e18e",
+    "url": "/pnp_tasks_taeha/index.html"
+  },
+  {
+    "revision": "a0b264b05069a27c7a2f",
+    "url": "/pnp_tasks_taeha/static/css/main.20b5b58f.chunk.css"
+  },
+  {
+    "revision": "ea781eba0a354af0f312",
+    "url": "/pnp_tasks_taeha/static/js/2.f3b6ba27.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/pnp_tasks_taeha/static/js/2.f3b6ba27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0b264b05069a27c7a2f",
+    "url": "/pnp_tasks_taeha/static/js/main.07fdca2b.chunk.js"
+  },
+  {
+    "revision": "c11d8b807a43f33cc4cd",
+    "url": "/pnp_tasks_taeha/static/js/runtime-main.c903dc9a.js"
+  },
+  {
+    "revision": "bc4c0ab82b59307389330d0f70cb01e5",
+    "url": "/pnp_tasks_taeha/static/media/dice_five.bc4c0ab8.png"
+  },
+  {
+    "revision": "09045f8beb65fea9901c3cea68756c3c",
+    "url": "/pnp_tasks_taeha/static/media/dice_four.09045f8b.png"
+  },
+  {
+    "revision": "96c2bb1549991f385fdaf5be88cf639d",
+    "url": "/pnp_tasks_taeha/static/media/dice_roll.96c2bb15.gif"
+  },
+  {
+    "revision": "a5a9c4e59c9a77332beb14d55ffa76c8",
+    "url": "/pnp_tasks_taeha/static/media/dice_six.a5a9c4e5.png"
+  },
+  {
+    "revision": "dccb94976b6170a1370e63b2d77d4823",
+    "url": "/pnp_tasks_taeha/static/media/dice_three.dccb9497.png"
+  },
+  {
+    "revision": "f5e7f316b29e5b4a32cd524375f42cb8",
+    "url": "/pnp_tasks_taeha/static/media/dice_two.f5e7f316.png"
+  },
+  {
+    "revision": "930f377bcbd4f0586227031025bb4011",
+    "url": "/pnp_tasks_taeha/static/media/rolling_dice.930f377b.gif"
+  }
+]);
